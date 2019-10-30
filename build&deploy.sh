87c33e29aa76dd073f9d4249477792223f625c0c
sanghangning <wb.sanghangning@mesg.corp.netease.com>
2019-10-30 16:27:24 +0800

    v0.0.7 add photolist update gta5

--- build&deploy.sh
+++ build&deploy.sh
@@ -1,10 +1,10 @@
 ###!/usr/bin/env sh
 
 ### 确保脚本抛出遇到的错误
-# set -e
+set -e
 
 ### 生成静态文件
-# npm run build
+npm run build
 
 ### 进入生成的文件夹
 cd docs/.vuepress/dist
---
+++ docs/.vuepress/components/base/photolist.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="photo-list">
+    <el-image
+      :key="item"
+      :previewSrcList="imgsArr"
+      :src="item"
+      fit="cover"
+      lazy
+      v-for="item in imgsArr"
+    >
+      <div class="image-slot" slot="error">
+        <i class="el-icon-picture-outline"></i>
+      </div>
+      <div class="loading-image-slot" slot="placeholder">
+        <i class="el-icon-loading"></i>
+      </div>
+    </el-image>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    number: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      imgsArr: []
+    }
+  },
+  mounted() {
+    for (let i = 1; i < this.number; i++) {
+      this.imgsArr.push(
+        `https://cdn.chenyingshuang.cn/${this.value}${i}.jpg?imageMogr2/auto-orient`
+      )
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.photo-list {
+  > div {
+    width: 340px;
+    height: 200px;
+    padding: 10px;
+  }
+  .loading-image-slot {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+@media screen and (max-width: 900px) {
+  .photo-list {
+    > div {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
